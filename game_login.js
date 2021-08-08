7d2e4b388c01fe9febabd125d2852541ae6ac591
parnavi10 <80200318+parnavi10@users.noreply.github.com>
2021-08-08 17:58:26 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,36 @@
+function adduser() {
+    player1_name = document.getElementById("player1_name").value;
+    player2_name = document.getElementById("player2_name").value;
+
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+
+    window.location.replace("game_page.html");
+}
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+
+player1_score = 0;
+player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + " :";
+document.getElementById("player2_name").innerHTML = player2_name + " :";
+
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+
+function send() {
+    number1 = document.getElementById("number_1").value;
+    number2 = document.getElementById("number_2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+    question_word = "<h5>" + number1 + " X " + number2 + "</h5>";
+    input_box = "Answer : <input type='text' id='input_check_box'>";
+    check_button = "<button class='btn btn-info' onclick='check()'>Check</button>";
+    row = question_word + input_box + check_button;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("number_1").value = "";
+    document.getElementById("number_2").value = "";
+}
